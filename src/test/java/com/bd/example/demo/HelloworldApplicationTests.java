198e4192f5679cb582ef437b3a5743421a7898a8
yaong <yaong@yaong-opcl>
2023-05-24 15:04:03 +0900

    change Jenkinsfile

---
+++ src/test/java/com/bd/example/demo/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
